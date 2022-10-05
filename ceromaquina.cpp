8a96b4afa7022b43d6c397ba7d915481fcdc3913
gokrusk <nigelljama@gmail.com>
2022-10-04 21:27:12 -0500

    ceromaquina

--- ceromaquina.cpp
+++ ceromaquina.cpp
@@ -2,17 +2,17 @@
 
 #include <iostream>
 using namespace std;
-	void xdd(float x, float a);
-	void xd(double x, double a);
+	void ceromaqnf(float x, float a);
+	void ceromaqnd(double x, double a);
 int main()
 {
 	float x = 1, a = 0;
 	double y = 1, b = 0;
-	xdd(x,a);
-	xd(y,b);
+	ceromaqnf(x,a);
+	ceromaqnd(y,b);
 	return 0;
 }
-void xdd(float x, float a){
+void ceromaqnf(float x, float a){
 	int i = 0;
 	do{
 	x /= 2;
@@ -23,7 +23,7 @@ void xdd(float x, float a){
 	cout<<"N veces: "<<i<<endl;
 	cout<<"El cero' es: "<<x<<endl;
 }
-void xd(double x, double a){
+void ceromaqnd(double x, double a){
 	int i = 0;
 	do{
 	x /= 2;
