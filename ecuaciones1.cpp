68d1a95a45b519f952651a30fff815fe2e95a8d7
Gokrusk <nigelljama@gmail.com>
2022-11-22 10:44:37 -0500

    ecuaciones

--- ecuaciones1.cpp
+++ ecuaciones1.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <math.h>
 using namespace std;
 
 int main()
@@ -6,28 +7,35 @@ int main()
 	float funR(float x);
 	float a, b, h, x1, x2, sg;
 	bool aux = false;
-	cout<<"Ingrese a: ";
-	cin>>a;
-	cout<<"Ingrese b: ";
-	cin>>b;
-	cout<<"Ingrese h (0.1): ";
-	cin>>h;
+	cout << "Ingrese a: ";
+	cin >> a;
+	cout << "Ingrese b: ";
+	cin >> b;
+	cout << "Ingrese h (0.1): ";
+	cin >> h;
 	x1 = a;
-	while(aux == false)
+	while (aux == false && x2 < b)
 	{
 		x2 = x1 + h;
-		sg = funR(x1)*funR(x2);
-		if(sg > 0)
+		sg = funR(x1) * funR(x2);
+		if (sg > 0)
 		{
 			x1 = x2;
-		}else
+		}
+		else
 		{
 			aux = true;
 		}
 	}
-	cout<<"La solucion aproximada es: "<<(x1+x2)/2;
+	if (aux == false)
+	{
+		cout << "No se encontr\242 soluci\242n" << endl;
+	}
+	else
+		cout << "La soluci\242n aproximada es: " << (x1 + x2) / 2 << endl;
+	return 0;
 }
 float funR(float x)
 {
-	return 2*x-14;
+	return exp(x) + x - 2;
 }
---
+++ ecuaciones2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float funR(float x);
+    float a, b, c, e, sg, x1, x2;
+    cout << "Ingrese a: ";
+    cin >> a;
+    cout << "Ingrese b: ";
+    cin >> b;
+
+    sg = funR(a) * funR(b);
+    if (sg > 0)
+    {
+        cout << "No se encuentra soluci\242 en ese intervalo" << endl;
+    }
+    else
+    {
+        x1 = a;
+        x2 = b;
+        e = (x2 - x1) / 2;
+        while (e >= 0.0002);
+        {
+            c = (x1 + x2) / 2;
+            sg = funR(x1) * funR(c);
+            if (sg < 0)
+            {
+                x2 = c;
+            }
+            else
+                x1 = c;
+            e = (x2 - x1) / 2;
+        } 
+    }
+    cout << "La solucion aproximada es: " << c << endl;
+    return 0;
+}
+float funR(float x)
+{    return exp(x) + x - 2;
+}
