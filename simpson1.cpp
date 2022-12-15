a65dc8b76a818a88cf3b175fb8e42463d4737eec
gokrusk <nigelljama@gmail.com>
2022-12-14 21:38:34 -0500

    simpson

--- simpson1.cpp
+++ simpson1.cpp
@@ -43,14 +43,14 @@ float formula(float a, float b, float c, float lg)
     else
     {
         n = trunc(lg) + 1;  // calculando las cantidad de divisiones que tendrá la integral
-        h = lg / n; // 
+        h = lg / n; // se calcula la primera division de la integral
         for (int i = 1; i <= n; i++)
         {
-            b = a + h;
-            c = a + (h / 2);
-            s += (((b - a) / 6) * (funX(a) + 4 * funX(c) + funX(b)));
-            a = b;
+            b = a + h;  // el valor de b cambia de acuerdo a la division
+            c = a + (h / 2);    // el valor de c es el punto medio entre a y la division h
+            s += (((b - a) / 6) * (funX(a) + 4 * funX(c) + funX(b)));   // se realiza la sumatoria
+            a = b;  //el valor de a avanza
         }
     }
-    return s;
+    return s;   // se retorna el valor aproximado de la integral
 }
