3da0a1a365318a1c55f551bc4587aa7c1a377acc
gokrusk <nigelljama@gmail.com>
2022-12-20 10:24:49 -0500

    ecuaciones

--- PARCIAL 2/newton2.cpp
+++ PARCIAL 2/newton2.cpp
@@ -5,24 +5,28 @@ using namespace std;
 int main()
 {
     float funx(float x);
-    float n, x0, x1, dx;
+    float n, x0, x1, dx, d;
+    bool aux = false;
     cout << "Ingrese cantidad de iteraciones: ";
     cin >> n;
     cout << "Ingrese punto inicial: ";
     cin >> x0;
     for (int i = 1; i <= n; i++)
     {
-        dx = (funx(x0+0.0001)-funx(x0))/0.0001;         //calculo derivada
+        dx = (funx(x0 + 0.0001) - funx(x0)) / 0.0001; // calculo derivada
         x1 = x0 - (funx(x0) / dx);
-        x0 = x1;
+        d = abs(x1 - x0);
     }
-
-    cout << "La solucion aproximada es: " << x1 << endl;
+    if (d < 0.01)
+    {
+        cout << "La solucion aproximada es: " << x1 << endl;
+    }
+    else
+        cout << "El algoritmo no encontro la raiz" << endl;
     return 0;
 }
 float funx(float x)
 {
-    //return 500*exp(-0.0248*x) - 5;
-    return 1/sqrt(1+pow(x,2));
-
+    // return 500*exp(-0.0248*x) - 5;
+    return pow(x, 3) + 3;
 }
---
+++ PARCIAL 2/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+if(x0 == x1)
+        {
+            aux = true;
+        }else
+            x0 = x1;
